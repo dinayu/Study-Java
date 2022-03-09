a92802ae93322edf013aedb0347890824004845e
dinayu <dinadia146@gmail.com>
2022-03-09 13:37:55 +0700

    add

---
+++ BelajarJavaku.java
@@ -0,0 +1,21 @@
+
+
+public class BelajarJavaku {
+  public static void main(String[] args){
+      
+      System.out.println("Panggil Method ee nde sini");
+      System.out.println("Buat methodnya di bawah seperti gambar ini");
+      
+  }
+
+  
+public static boolean kelinciPaman(boolean tidurM, boolean tidurT) {
+    if(tidurM & tidurT) {
+        return true;
+    } else if(!tidurM && !tidurT) {
+        return true;
+    } else { 
+        return false;
+    }
+}
+
